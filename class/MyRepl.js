87421ab460cbe2ef95caef93dda58cdadc80466e
masyuko0222 <masyuko0222@gmail.com>
2023-09-16 22:27:13 +0900

    repl終了後に、作成したtmpDirは自動で削除

--- class/MyRepl.js
+++ class/MyRepl.js
@@ -1,9 +1,14 @@
 import repl from "repl";
 
 export class MyRepl {
-  static start(prompt = ""){
-    repl.start({
-      prompt: prompt
-    })
+  static start(prompt = "", dir = {}) {
+    const replServer = repl.start({
+      prompt: prompt,
+    });
+
+    replServer.on("exit", () => {
+      console.log("See you later...");
+      dir.cleanupCallback();
+    });
   }
 }
--- main.js
+++ main.js
@@ -1,12 +1,10 @@
 import { MyRepl } from "./class/MyRepl.js";
 import { TmpDir } from "./class/TmpDir.js";
 
-async function main() {
-  // dir has own path and clean up function.
-  //const dir = await TmpDir.create();
-
+async function run() {
+  const dir = await TmpDir.create();
   const prompt = "Quick npm >";
-  MyRepl.start(prompt)
+  MyRepl.start(prompt, dir);
 }
 
-main();
+run();
