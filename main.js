68225cfb73548f726686fdd5886f4794d1487ace
masyuko0222 <masyuko0222@gmail.com>
2023-09-17 21:33:56 +0900

    Tmpdirにpackage.jsonを作り、type: moduleを追加する

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ import { TmpDir } from "./class/TmpDir.js";
 
 async function run() {
   const dir = await TmpDir.create();
+  console.log(dir.path)
   const prompt = "Quick npm >";
   MyRepl.start(prompt, dir);
 }
--- module/Helper.js
+++ module/Helper.js
@@ -15,16 +15,14 @@ export function init(dir) {
 
 // private
 async function addTypeModule(package_json) {
-  const data = await fsPromise.readFile(package_json.path, (err, data) => {
-    if (err) throw err;
-    return data;
+  const data = await fsPromise.readFile(package_json.path, {
+    encoding: "utf-8",
   });
+
   const json = JSON.parse(data);
   json.type = "module";
 
-  console.log(json);
-
-  //const newJson =
-  //fs.writeFileSync(package_json.path, newJson)
-  // ???
+  fs.writeFileSync(package_json.path, JSON.stringify(json, null, 2), {
+    encoding: "utf-8",
+  });
 }
